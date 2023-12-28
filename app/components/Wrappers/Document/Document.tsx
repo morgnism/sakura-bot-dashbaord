98d3c6e76f3a2baf1b9b8aa2b078135df276dfad
Morgan Worrell <hello@morganism.codes>
2023-12-28 16:58:24 -0700

    adds document wrapper

---
+++ app/components/Wrappers/Document/Document.tsx
@@ -0,0 +1,36 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react';
+import { PropsWithChildren } from 'react';
+import { IS_DEVELOP } from '~/lib/config';
+
+const Document = ({
+  children,
+  title,
+}: PropsWithChildren<{ title?: string }>) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+        {title ? <title>{title}</title> : null}
+      </head>
+      <body>
+        {children}
+
+        <ScrollRestoration />
+        <Scripts />
+        {/*Enable live reload in development environment only, not production */}
+        {IS_DEVELOP ? <LiveReload /> : null}
+      </body>
+    </html>
+  );
+};
+
+export default Document;
