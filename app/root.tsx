6b43ce42d4ae5eb8bd433616796a63ce9dba023a
Morgan Worrell <hello@morganism.codes>
2023-12-28 17:02:02 -0700

    simplifies root page

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,39 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { cssBundleHref } from '@remix-run/css-bundle';
+import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node';
+import { Outlet } from '@remix-run/react';
+import Document from './components/Wrappers/Document';
+import Layout from './components/Wrappers/Layout';
+import styles from './global.css';
+import { authenticator } from './server/auth.server';
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  ...(cssBundleHref
+    ? [{ rel: 'stylesheet', href: cssBundleHref }]
+    : [{ rel: 'stylesheet', href: styles }]),
 ];
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return await authenticator.isAuthenticated(request);
+};
 
 export default function App() {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
+    <Document title="My Remix App">
+      <Layout>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
+      </Layout>
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: any }) {
+  console.log(error);
+  return (
+    <Document>
+      <Layout>
+        <h1>There was an Error</h1>
+        <p>{error?.message}</p>
+      </Layout>
+    </Document>
   );
 }
