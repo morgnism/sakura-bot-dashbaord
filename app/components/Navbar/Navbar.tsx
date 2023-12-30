0e9886444cc7ba69189b9b8ad3c1c436c75aa7a3
Morgan Worrell <hello@morganism.codes>
2023-12-29 19:31:15 -0700

    updates navbar based on roues

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,29 +1,53 @@
-import { Link } from '@remix-run/react';
+import { Link, useLocation } from '@remix-run/react';
+import classNames from 'classnames';
+import { hasChildPath } from '~/utils/path-utils';
+import Divider from '../Divider';
+import Logo from '../Logo';
 import ProfileMenuButton from '../ProfileMenu';
+import MobileButton from './MobileButton';
+import NotificationBell from './NotificationBell';
 
-const Navbar = () => (
-  <nav
-    className="mx-auto flex max-w-7xl items-center justify-between gap-6 p-6 lg:px-8"
-    aria-label="global"
-  >
-    <div className="flex items-center gap-12">
-      <Link to="/" className="-m-[0.375rem] p-[0.375rem]">
-        <span className="sr-only">Your Company</span>
-        <img
-          className="h-8 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&amp;shade=600"
-          alt=""
-        />
-      </Link>
-      <Link
-        to="/dashboard"
-        className="text-sm font-semibold leading-6 text-[#111827] dark:text-white"
-      >
-        Dashboard
-      </Link>
-    </div>
-    <ProfileMenuButton />
-  </nav>
-);
+const Navbar = () => {
+  const { pathname } = useLocation();
+  const isDashboardPath = hasChildPath(pathname);
+
+  return (
+    <nav
+      className={classNames([
+        'mx-auto flex max-w-7xl items-center justify-between gap-6 h-16 lg:px-8',
+        isDashboardPath ? 'bg-[#111827] border-b border-[#e5e7eb] shadow' : '',
+      ])}
+      aria-label="global"
+    >
+      <div className="flex items-center gap-8 lg:gap-12">
+        {isDashboardPath ? (
+          <>
+            <MobileButton onClick={() => {}} />
+            <Divider className="lg:hidden" />
+          </>
+        ) : (
+          <>
+            <Logo />
+            <Link
+              to="/dashboard"
+              className="text-sm font-semibold leading-6 text-[#111827] dark:text-white"
+            >
+              Dashboard
+            </Link>
+          </>
+        )}
+      </div>
+      <div className="flex gap-x-4 items-center self-stretch lg:gap-x-6">
+        {isDashboardPath && (
+          <>
+            <NotificationBell onClick={() => {}} />
+            <Divider className="lg:block md:hidden" />
+          </>
+        )}
+        <ProfileMenuButton />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
