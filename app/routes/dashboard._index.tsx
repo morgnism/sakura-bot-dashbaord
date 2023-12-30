11ba150ce41c625c16a2b2f52e227737336b631f
Morgan Worrell <hello@morganism.codes>
2023-12-29 19:33:40 -0700

    wraps main dashboard view with global layout

--- app/routes/dashboard._index.tsx
+++ app/routes/dashboard._index.tsx
@@ -1,5 +1,6 @@
 import { LoaderFunctionArgs } from '@remix-run/node';
 import { Link, useLoaderData } from '@remix-run/react';
+import MainLayout from '~/components/Wrappers/MainLayout';
 import { AppRoutes } from '~/lib/constants';
 import { authenticator } from '~/server/auth.server';
 import { imageUrlBuilder } from '~/utils/image-url-builder';
@@ -14,31 +15,33 @@ export default function DashboardServersPage() {
   const user = useLoaderData<typeof loader>();
 
   return (
-    <div className="relative z-[1] mx-auto mt-10 flex max-w-[948px] flex-col items-center px-10">
-      <h1 className="text-5xl font-bold text-white">Select a Server</h1>
-      <div className="mb-16 mt-14 grid w-full grid-cols-[1fr_1fr_1fr] gap-10">
-        {user.guilds.map(({ id, icon, name }) => (
-          <Link key={id} to={`/dashboard/${id}`}>
-            <div className="w-full">
-              <div className="relative flex h-40 w-full items-center justify-center overflow-hidden">
-                <div className="absolute inset-0 z-[1] scale-[1.4] opacity-30 blur-[10px]"></div>
-                <img
-                  className="relative z-[2] h-40 w-40 shrink-0 rounded-full border-2 border-solid border-[#f2f4fb]"
-                  src={imageUrlBuilder(id, icon, { resource: 'icons' })}
-                  alt={`${name} Logo`}
-                />
-              </div>
-              <div className="mt-4 flex items-start justify-between">
-                <div className="mr-3">
-                  <h3 className="overflow-hidden text-ellipsis p-0 text-center text-base font-bold leading-6 text-[#f2f4fb]">
-                    {name}
-                  </h3>
+    <MainLayout>
+      <div className="relative z-[1] mx-auto mt-10 flex max-w-[948px] flex-col items-center px-10">
+        <h1 className="text-5xl font-bold text-white">Select a Server</h1>
+        <div className="mb-16 mt-14 grid w-full grid-cols-[1fr_1fr_1fr] gap-10">
+          {user.guilds.map(({ id, icon, name }) => (
+            <Link key={id} to={`/dashboard/${id}`}>
+              <div className="w-full">
+                <div className="relative flex h-40 w-full items-center justify-center overflow-hidden">
+                  <div className="absolute inset-0 z-[1] scale-[1.4] opacity-30 blur-[10px]"></div>
+                  <img
+                    className="relative z-[2] h-40 w-40 shrink-0 rounded-full border-2 border-solid border-[#f2f4fb]"
+                    src={imageUrlBuilder(id, icon, { resource: 'icons' })}
+                    alt={`${name} Logo`}
+                  />
+                </div>
+                <div className="mt-4 flex items-start justify-between">
+                  <div className="mr-3">
+                    <h3 className="overflow-hidden text-ellipsis p-0 text-center text-base font-bold leading-6 text-[#f2f4fb]">
+                      {name}
+                    </h3>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+    </MainLayout>
   );
 }
