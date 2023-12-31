5578d43d892ac77a65e59d1de7452ff485d35218
Morgan Worrell <hello@morganism.codes>
2023-12-31 15:54:29 -0700

    adds feature state to manage feature toggling

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,5 +1,3 @@
-// export const CLIENT_REDIRECT_URI = '/api/auth/signin?callbackUrl=';
-
 const API_VERSION = '10';
 
 export const DISCORD_BASE_URL = `https://discord.com/api/v${API_VERSION}`;
@@ -10,6 +8,10 @@ export const enum AppRoutes {
   AUTH = '/auth',
   LOGIN = '/login',
   DASHBOARD = '/dashboard',
+  WELCOME = '/welcome',
+  LEAVE = '/leave',
+  AUTO_ROLES = '/auto_roles',
+  MODERATOR = '/moderator',
 }
 
 export const RegisteredModules = {
@@ -19,4 +21,71 @@ export const RegisteredModules = {
   moderationConfig: true,
 };
 
-export const RegisteredModuleKeys = Object.keys(RegisteredModules);
+const FeaturesKeys = {
+  WELCOME: 'welcomeConfig',
+  LEAVE: 'leaveConfig',
+  AUTOROLE: 'autoroleConfig',
+  MODERATION: 'moderationConfig',
+};
+
+const FeatureDefaults = {
+  isAvailable: true,
+  isEnabled: true,
+  isPremiumFeature: false,
+};
+
+export const enum FeatureTypes {
+  SERVER_MANAGEMENT = 'Server Management',
+}
+
+export const AvailableFeatures = new Map<string, typeof FeatureDefaults>();
+
+export const setMap = () => {
+  Object.values(FeaturesKeys).map((feature) =>
+    AvailableFeatures.set(feature, FeatureDefaults)
+  );
+};
+
+setMap();
+
+// Navigation Menus
+
+export const MAIN_SIDEBAR_MENU = [
+  { to: '', label: 'Dashboard' },
+  { to: './settings', label: 'Settings' },
+];
+
+export const Features = [
+  {
+    moduleKey: 'welcomeConfig',
+    to: `./${AppRoutes.WELCOME}`,
+    label: 'Welcome',
+    enabled: false,
+    weight: 1,
+    type: FeatureTypes.SERVER_MANAGEMENT,
+  },
+  {
+    moduleKey: 'leaveConfig',
+    to: `./${AppRoutes.LEAVE}`,
+    label: 'Goodbye',
+    enabled: false,
+    weight: 2,
+    type: FeatureTypes.SERVER_MANAGEMENT,
+  },
+  {
+    moduleKey: 'autoroleConfig',
+    to: `./${AppRoutes.AUTO_ROLES}`,
+    label: 'Auto Roles',
+    enabled: false,
+    weight: 3,
+    type: FeatureTypes.SERVER_MANAGEMENT,
+  },
+  {
+    moduleKey: 'moderationConfig',
+    to: `./${AppRoutes.MODERATOR}`,
+    label: 'Moderator',
+    enabled: false,
+    weight: 4,
+    type: FeatureTypes.SERVER_MANAGEMENT,
+  },
+];
--- app/routes/dashboard.$serverId.tsx
+++ app/routes/dashboard.$serverId.tsx
@@ -1,41 +1,44 @@
-import { LoaderFunctionArgs, json } from '@remix-run/node';
-import { NavLink, Outlet, useLoaderData } from '@remix-run/react';
+import { LoaderFunctionArgs } from '@remix-run/node';
+import { NavLink, Outlet } from '@remix-run/react';
 import invariant from 'tiny-invariant';
 import { getAllConfigs } from '~/api/guilds.server';
 import Logo from '~/components/Logo';
 import Navbar from '~/components/Navbar';
 import RouteLayout from '~/components/Wrappers/RouteLayout';
-import { RegisteredModuleKeys } from '~/lib/constants';
+import {
+  AvailableFeatures,
+  FeatureTypes,
+  Features,
+  MAIN_SIDEBAR_MENU,
+} from '~/lib/constants';
+import { GuildConfigs } from '~/type';
+
+export const initFeatures = (allSettings: GuildConfigs) => {
+  // map config settings
+  Object.entries(allSettings).map(([key, value]) => {
+    const feature = AvailableFeatures.get(key);
+    if (feature) {
+      const isEnabled = (value as { enabled: boolean }).enabled;
+      AvailableFeatures.set(key, {
+        ...feature,
+        isEnabled,
+      });
+    }
+  });
+};
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   invariant(params.serverId, 'Missing serverId param');
-  const guildConfig = await getAllConfigs(params.serverId);
-
-  if (!guildConfig) {
-    throw new Response('Not Found', { status: 404 });
-  }
-
-  const moduleConfigs = Object.fromEntries(
-    Object.entries(guildConfig).filter(([key]) =>
-      RegisteredModuleKeys.includes(key)
-    )
-  );
-  return json({ guildConfig, moduleConfigs });
+  const allSettings = await getAllConfigs(params.serverId);
+  initFeatures(allSettings);
+  return allSettings;
 };
 
-const MAIN_MENU_ITEMS = [
-  { to: '', label: 'Dashboard' },
-  { to: './settings', label: 'Settings' },
-];
-
-const SERVER_MANAGEMENT_NAV = [
-  { key: 'welcomeConfig', to: './welcome', label: 'Welcome', weight: 1 },
-  { key: 'leaveConfig', to: './leave', label: 'Goodbye', weight: 2 },
-  { key: 'autoroleConfig', to: './auto_roles', label: 'Auto Roles', weight: 3 },
-  { key: 'moderationConfig', to: './moderator', label: 'Moderator', weight: 4 },
-];
-
 export default function DashboardServerLayout() {
+  const SERVER_MANAGEMENT_SIDEBAR_MENU = Features.filter(
+    (module) => module.type === FeatureTypes.SERVER_MANAGEMENT
+  );
+
   return (
     <>
       <div
@@ -48,7 +51,7 @@ export default function DashboardServerLayout() {
             <ul role="list" className="flex flex-1 flex-col gap-y-7">
               <li>
                 <ul role="list" className="-m-2 abo">
-                  {MAIN_MENU_ITEMS.map(({ label, to }, i) => (
+                  {MAIN_SIDEBAR_MENU.map(({ label, to }, i) => (
                     <li key={i}>
                       <NavLink
                         to={to}
@@ -79,34 +82,6 @@ export default function DashboardServerLayout() {
                       </NavLink>
                     </li>
                   ))}
-                  <li>
-                    <NavLink
-                      to="./settings"
-                      className="hover:bg-[#1f2937] text-[#9ca3af] hover:text-white bqg flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                        className="text-[#9ca3af] hover:text-white h-6 w-6 shrink-0"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
-                        ></path>
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        ></path>
-                      </svg>
-                      Settings
-                    </NavLink>
-                  </li>
                 </ul>
               </li>
               <li>
@@ -114,7 +89,7 @@ export default function DashboardServerLayout() {
                   Server Management
                 </div>
                 <ul role="list" className="-m-2 mt-2">
-                  {SERVER_MANAGEMENT_NAV.map(({ label, to }, i) => (
+                  {SERVER_MANAGEMENT_SIDEBAR_MENU.map(({ label, to }, i) => (
                     <li key={i}>
                       <NavLink
                         to={to}
