7e7ac0d745a15e1210822d00fffac7ff715d56d5
Morgan Worrell <hello@morganism.codes>
2023-12-31 23:14:09 -0700

    fixes detached form on logout in dropdown menu

--- app/components/ProfileMenu/ProfileFlyoutMenu.tsx
+++ app/components/ProfileMenu/ProfileFlyoutMenu.tsx
@@ -15,17 +15,17 @@ const ProfileFlyoutMenu = ({ menuRef, closeMenu }: ProfileFlyoutMenuProps) => (
     <h4 className="p-3 text-xs font-bold uppercase text-[#f2f4fb]">
       Servers owners
     </h4>
-    <div className="relative cursor-pointer whitespace-nowrap rounded-lg bg-transparent px-3 py-2.5 text-sm text-[#9195ab] transition-all hover:bg-[#1f2129]">
-      <Link to="/dashboard" onClick={closeMenu}>
+    <Link to="/dashboard" onClick={closeMenu}>
+      <div className="relative cursor-pointer whitespace-nowrap rounded-lg bg-transparent px-3 py-2.5 text-sm text-[#9195ab] transition-all hover:bg-[#1f2129]">
         My servers
-      </Link>
-    </div>
+      </div>
+    </Link>
     <div className="px-3 py-2.5">
       <div className="h-px w-full bg-[#313442]"></div>
     </div>
     <Form action="/logout" method="post">
-      <div className="relative cursor-pointer whitespace-nowrap rounded-lg bg-transparent px-3 py-2.5 text-sm text-[#9195ab] transition-all hover:bg-[#1f2129]">
-        <Button onClick={closeMenu}>Logout</Button>
+      <div className="relative cursor-pointer whitespace-nowrap rounded-lg bg-transparent text-sm text-[#9195ab] transition-all hover:bg-[#1f2129]">
+        <Button className="w-full px-3 py-2.5 text-left">Logout</Button>
       </div>
     </Form>
   </div>
