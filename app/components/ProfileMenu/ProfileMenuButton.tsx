e65090e132d1d14243d9bbaf39fe1833a421e986
Morgan Worrell <hello@morganism.codes>
2024-01-01 14:03:37 -0700

    fixes sign in button

--- app/components/ProfileMenu/ProfileMenuButton.tsx
+++ app/components/ProfileMenu/ProfileMenuButton.tsx
@@ -8,22 +8,6 @@ import DiscordSignInButton from './DiscordSignInButton';
 import ProfileFlyoutMenu from './ProfileFlyoutMenu';
 import ProfileFlyoutMenuControl from './ProfileFlyoutMenuControl';
 
-type SocialButtonProps = {
-  provider: SocialsProvider;
-  label: string;
-};
-
-const SocialButton: React.FC<SocialButtonProps> = ({ provider, label }) => (
-  <Form action={`${AppRoutes.AUTH}/${provider}`} method="post">
-    <DiscordSignInButton
-      className="rounded-md bg-[#4f46e5] text-sm font-semibold text-white shadow-sm hover:bg-[hsl(239,84%,67%)] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#4f46e5] relative mx-auto flex w-full shrink-0 items-center gap-1.5 overflow-hidden px-4 py-2 transition-all duration-200 active:bg-opacity-40 disabled:cursor-not-allowed disabled:opacity-50 md:mx-0 md:w-auto"
-      type="submit"
-    >
-      {label}
-    </DiscordSignInButton>
-  </Form>
-);
-
 const ProfileMenuButton = () => {
   const [isOpen, setOpen] = useState(false);
   const user = useOptionalUser();
@@ -49,10 +33,12 @@ const ProfileMenuButton = () => {
           )}
         </div>
       ) : (
-        <SocialButton
-          provider={SocialsProvider.DISCORD}
-          label="Sign In With Discord"
-        />
+        <Form
+          action={`${AppRoutes.AUTH}/${SocialsProvider.DISCORD}`}
+          method="post"
+        >
+          <DiscordSignInButton />
+        </Form>
       )}
     </>
   );
