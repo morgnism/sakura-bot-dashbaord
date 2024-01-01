64fe975b03b469df286806a493aa505f6e7e6b66
Morgan Worrell <hello@morganism.codes>
2023-12-31 22:38:42 -0700

    adds feature toggling to server dashboard

--- app/routes/dashboard.$serverId._index.tsx
+++ app/routes/dashboard.$serverId._index.tsx
@@ -1,11 +1,52 @@
-import { LoaderFunctionArgs, json } from '@remix-run/node';
+import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 import invariant from 'tiny-invariant';
+import { getFeatures, updateFeature } from '~/api/guilds.server';
+import ToggleCard from '~/components/ToggleCard';
+import { Feature, FeatureMap } from '~/lib/constants';
+import { FeatureConfigs } from '~/type';
+
+const mapEnabledFeatures = (configs: FeatureConfigs[]) =>
+  configs.reduce((a: Array<Feature & { key: string }>, { name, enabled }) => {
+    const feature = FeatureMap.get(name);
+    if (feature) {
+      a.push({ ...feature, key: name, enabled });
+    }
+    return a;
+  }, []);
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   invariant(params.serverId, 'Missing serverId param');
-  return json({});
+  return await getFeatures(params.serverId);
+};
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  invariant(params.serverId, 'Missing serverId param');
+  const formData = await request.formData();
+  return updateFeature(params.serverId, {
+    name: formData.get('name') as string,
+    enabled: formData.get('enabled') === 'true',
+  });
 };
 
 export default function DashboardServerHomePage() {
-  return <div className="px-4 md:px-6 lg:px-8">DashboardServerHomePage</div>;
+  const configs = useLoaderData<typeof loader>();
+  const features = mapEnabledFeatures(configs);
+
+  return (
+    <div className="px-4 md:px-6 lg:px-8">
+      DashboardServerHomePage
+      <div className="mb-16 mt-14 grid w-full grid-cols-[1fr_1fr_1fr] gap-10">
+        {features.map(({ key, to, label, enabled }) => (
+          <ToggleCard
+            key={key}
+            name={key}
+            to={to}
+            label={label}
+            enabled={enabled}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
