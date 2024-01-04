16eb40fa61fae4893d006d7a78cec77afd21d786
Morgan Worrell <hello@morganism.codes>
2024-01-03 18:06:25 -0700

    updates repsonsive grid columns

--- app/routes/dashboard.$serverId._index.tsx
+++ app/routes/dashboard.$serverId._index.tsx
@@ -97,14 +97,14 @@ export default function DashboardServerHomePage() {
       </div>
       <Form {...form}>
         <fetcher.Form onSubmit={form.handleSubmit(onSubmit)}>
-          <div className="mb-16 mt-14 grid w-full md:grid-cols-3 grid-cols-2 gap-10">
+          <div className="mb-16 mt-14 grid w-full md:grid-cols-3 xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-10">
             {features.map(([key, feature]) => (
               <FormField
                 key={key}
                 control={form.control}
                 name={key}
                 render={({ field }) => (
-                  <FormItem className="flex flex-col items-start lg:flex-row lg:items-center justify-between rounded-lg border border-[#27272a] p-4">
+                  <FormItem className="flex flex-row items-center justify-between rounded-lg border border-[#27272a] p-4">
                     <div className="space-y-0.5 md:pr-2 pr-0">
                       <FormLabel className="text-base">
                         <Link
