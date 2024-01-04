ec18300a97ec87edfbb792eff1924e371e33fb1e
Morgan Worrell <hello@morganism.codes>
2024-01-03 20:10:37 -0700

    updates profile dropdown menu

--- app/components/ProfileMenu/ProfileMenuButton.tsx
+++ app/components/ProfileMenu/ProfileMenuButton.tsx
@@ -1,37 +1,50 @@
-import { Form } from '@remix-run/react';
+import { Form, Link } from '@remix-run/react';
 import { useState } from 'react';
-import { SocialsProvider } from 'remix-auth-socials';
-import useOutsideClick from '~/hooks/useClickOutside';
 import { useOptionalUser } from '~/hooks/useUser';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+import ProfileFlyoutMenuControl from './ProfileFlyoutMenuControl';
+import { SocialsProvider } from 'remix-auth-socials';
 import { AppRoutes } from '~/lib/route';
 import DiscordSignInButton from './DiscordSignInButton';
-import ProfileFlyoutMenu from './ProfileFlyoutMenu';
-import ProfileFlyoutMenuControl from './ProfileFlyoutMenuControl';
 
 const ProfileMenuButton = () => {
   const [isOpen, setOpen] = useState(false);
   const user = useOptionalUser();
 
-  const menuRef = useOutsideClick(() => {
-    setOpen(false);
-  });
-
   return (
     <>
       {user ? (
-        <div className="relative z-10 ">
-          <ProfileFlyoutMenuControl
-            isMenuOpen={isOpen}
-            user={user}
-            openMenu={() => setOpen(true)}
-          />
-          {isOpen && (
-            <ProfileFlyoutMenu
-              menuRef={menuRef}
-              closeMenu={() => setOpen(false)}
-            />
-          )}
-        </div>
+        <DropdownMenu onOpenChange={setOpen}>
+          <DropdownMenuTrigger>
+            <ProfileFlyoutMenuControl isMenuOpen={isOpen} user={user} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-48">
+            <DropdownMenuLabel>Servers owners</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <Link className="w-full" to="/dashboard">
+                  My servers
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Form className="w-full" action="/logout" method="post">
+                  <button className="w-full text-left" type="submit">
+                    Logout
+                  </button>
+                </Form>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       ) : (
         <Form
           action={`${AppRoutes.AUTH}/${SocialsProvider.DISCORD}`}
