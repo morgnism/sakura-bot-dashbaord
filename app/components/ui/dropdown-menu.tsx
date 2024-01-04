57f27c17a67b1eb2f3278d834870a8970b44bf4f
Morgan Worrell <hello@morganism.codes>
2024-01-03 20:50:39 -0700

    fixes style collision

--- app/components/ui/dropdown-menu.tsx
+++ app/components/ui/dropdown-menu.tsx
@@ -159,7 +159,7 @@ const DropdownMenuSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DropdownMenuPrimitive.Separator
     ref={ref}
-    className={cn('-mx-1 my-1 h-px bg-zinc-100 bg-zinc-800', className)}
+    className={cn('-mx-1 my-1 h-px bg-zinc-800', className)}
     {...props}
   />
 ));
