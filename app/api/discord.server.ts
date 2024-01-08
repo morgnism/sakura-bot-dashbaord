551ec758dcbb8e11d60dd2439b7fedd3341fd94f
Morgan Worrell <hello@morganism.codes>
2024-01-07 23:54:35 -0700

    adds channel types

--- app/api/discord.server.ts
+++ app/api/discord.server.ts
@@ -2,6 +2,22 @@ import { PartialDiscordGuild } from 'remix-auth-socials';
 import { DISCORD_TOKEN } from '../lib/config';
 import { DISCORD_BASE_URL } from '../lib/constants';
 
+export enum GuildChannelTypes {
+  GUILD_TEXT = 0,
+  DM = 1,
+  GUILD_VOICE = 2,
+  GROUP_DM = 3,
+  GUILD_CATEGORY = 4,
+  GUILD_ANNOUNCEMENT = 5,
+  ANNOUNCEMENT_THREAD = 10,
+  PUBLIC_THREAD = 11,
+  PRIVATE_THREAD = 12,
+  GUILD_STAGE_VOICE = 13,
+  GUILD_DIRECTORY = 14,
+  GUILD_FORUM = 15,
+  GUILD_MEDIA = 16,
+}
+
 export type PartialGuildChannel = {
   id: string;
   last_message_id: string;
