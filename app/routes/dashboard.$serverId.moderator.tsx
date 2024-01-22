db0edc297a5ec1f147a5d52d79aa1a2b13038f40
Morgan Worrell <hello@morganism.codes>
2024-01-22 15:54:57 -0700

    initiates moderator page

---
+++ app/routes/dashboard.$serverId.moderator.tsx
@@ -0,0 +1,14 @@
+export default function ModeratorPage() {
+  return (
+    <div className="grid gap-6">
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center justify-between">
+          <h2 className="text-3xl font-bold tracking-tight">Moderator</h2>
+        </div>
+        <p className="text-zinc-400">
+          Set up commands and filters for unwanted behaviour.
+        </p>
+      </div>
+    </div>
+  );
+}
